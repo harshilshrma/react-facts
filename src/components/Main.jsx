ab9d263283679d9730ebead2f48bfdd9bcb63b77
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-11-25 00:52:22 +0530

    project done (final.png)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ export default function Main() {
     return (
         <main id="main">
             <h1 id="main-title">Fun facts about React</h1>
-            <ul>
+            <ul className="facts-list">
                 <li>Was first release in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 200K stars on GitHub</li>

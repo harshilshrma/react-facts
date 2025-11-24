42a02c56ab9b7a9c9b169110524450d7cfd8b1a4
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-11-24 21:54:11 +0530

    first view of the project (firstView.png)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
+import reactLogoSrc from './assets/react.svg'
 
 function App() {
-
   return (
-    <>
-      <button>Hello</button>
-      <h1>Hi hi hi hi hi hi</h1>
-    </>
+    <div id="container">
+      <img src={reactLogoSrc} width="50px"></img>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first release in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
   )
 }
 

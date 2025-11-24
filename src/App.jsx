1499f9f86734011b88b2e734f491b75ffdc4f46e
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-11-24 16:18:08 +0530

    project markup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <button>Hello</button>
+      <h1>Hi hi hi hi hi hi</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client';
+import './index.css'
+import { StrictMode } from 'react';
+import App from './App.jsx';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)

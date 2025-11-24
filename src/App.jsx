4b29b2031e1609b959f03de74576d0d9ec364997
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-11-25 00:36:32 +0530

    organised the code into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Main from './components/Main'
 
 function App() {
   return (
     <div id="container">
       <Navbar />
-      
-      <main id="main">
-        <h1 id="main-title">Fun facts about React</h1>
-        <ul>
-          <li>Was first release in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 200K stars on GitHub</li>
-          <li>Is maintained by Meta</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </main>
+      <Main />
     </div>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import './Main.css'
+
+export default function Main() {
+    return (
+        <main id="main">
+            <h1 id="main-title">Fun facts about React</h1>
+            <ul>
+                <li>Was first release in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
